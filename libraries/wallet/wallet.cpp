0fa6874a4bca1e8ab0bded29d2c67ab74fa98602
Edinarcoin developer <dev@e-dinarcoin.com>
2016-08-22 15:56:00 +0300

    changed fee on transfer from cli

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -2009,7 +2009,7 @@ public:
       xfer_op.from = from_id;
       xfer_op.to = to_id;
       xfer_op.amount = asset_obj->amount_from_string(amount);
-      xfer_op.fee = asset_obj->amount_from_string("100");
+      xfer_op.fee = asset_obj->amount_from_string("0.1");
       if( memo.size() )
          {
             xfer_op.memo = memo_data();
